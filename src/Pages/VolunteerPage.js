929bb8cdc552e19fa00806217e0aec92d2799e22
scribblemaniac <scribblemaniac@users.noreply.github.com>
2023-06-28 23:35:28 -0600

    Apply updates to the volunteer page

--- src/Pages/VolunteerPage.js
+++ src/Pages/VolunteerPage.js
@@ -9,34 +9,34 @@ function VolunteerPage() {
                 <section class="container-fluid px-0">
                 <div class="row mx-0">
                     <div class="col px-0">
-                        <img src="images/volunteer_images_welding.JPG" alt="Image description" class="img-fluid"/>
+                        <img src="images/volunteer_images_welding.JPG" alt="Close up image of welding" class="img-fluid" style={{width: "100%", height: "400px"}} />
                     </div>
-                    </div>
+                </div>
                 </section>
 
                 <section class="container mt-4">            
                     <div class="row">
                         <div class="col-6">
-                            <img src="images/volunteer_images_1.JPG" alt="Image description" class="img-fluid"/>
+                            <img src="images/volunteer_images_1.JPG" alt="Two people unloading a walker from the trunk of a yellow car" class="img-fluid" />
                             <h4>Volunteers meet with clients to discuss their needs and potential solutions</h4>
-                    </div>
-                    <div class="col-6">
-                        <img src="images/volunteer_images_2.JPG" alt="Image description" class="img-fluid"/>
-                        <h4>Volunteers often collaborate to solve complex issues at our fully outfitted machine shop (coffee & cookies provided!)</h4>
-                    </div>
+                        </div>
+                        <div class="col-6">
+                            <img src="images/volunteer_images_2.JPG" alt="Three men in a workshop looking at schematics on a whiteboard" class="img-fluid" />
+                            <h4>Volunteers often collaborate to solve complex issues at our fully outfitted machine shop (coffee & cookies provided!)</h4>
+                        </div>
                     </div>
                 </section>
 
                 <section class="container mt-4">
                     <div class="row">
-                    <div class="col-6">
-                        <img src="images/volunteer_images_3.JPG" alt="Image description" width="510" height="300" class="img-fluid"/>
-                        <h4>While others choose to work from home</h4>
-                    </div>
-                    <div class="col-6">
-                        <img src="images/volunteer_images_4.JPG" alt="Image description" width="510" height="300" class="img-fluid"/>
-                        <h4>The shop contains a variety of hand & machinist tools as well as plastic working tools </h4>
-                    </div>
+                        <div class="col-6">
+                            <img src="images/volunteer_images_3.JPG" alt="Woman in a workshop wearing safety glasses inspecting her project" class="img-fluid" />
+                            <h4>While others choose to work from home</h4>
+                        </div>
+                        <div class="col-6">
+                            <img src="images/volunteer_images_4.JPG" alt="Man in a workshop using a grinding wheel on a piece of metal causing sparks" class="img-fluid" />
+                            <h4>The shop contains a variety of hand & machinist tools as well as plastic working tools </h4>
+                        </div>
                     </div>
                 </section>
             </main>
