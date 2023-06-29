3defe67af38987055a3d2468d6bb3cea288ca4b2
scribblemaniac <scribblemaniac@users.noreply.github.com>
2023-06-28 23:24:23 -0600

    Apply updates to donation page

--- src/Donation.js
+++ src/Donation.js
@@ -1,21 +1,21 @@
 import { NavBar, Footer } from './GlobalComponents'
 
-function Donation(){
-    return(
+function Donation() {
+    return (
         <div className="App">
             <NavBar />
 
             <main>
-            <section class="container mt-4">
-                <h1>Donation</h1>
-            </section>
+                <section class="container mt-4">
+                    <h1>Donation</h1>
+                </section>
 
-            <section class="container mt-4">
-                <div id="donationContainer" >
-                <iframe id="donationIframe" 
-                    src="https://shawcharityclassic.com/charity/participating-charities/ability-workshop-society/"></iframe>
-                </div>
-            </section>
+                <section class="container mt-4">
+                    <div id="donationContainer" style={{width: "100%", height: "1150px", overflow: "hidden"}}>
+                    <iframe id="donationIframe" style={{width: "100%", height: "100000px", top: "-1200px", position: "relative", border: "none"}}
+                        src="https://shawcharityclassic.com/charity/participating-charities/ability-workshop-society/" title="Shaw Charity Classic Donation Page"></iframe>
+                    </div>
+                </section>
             </main>
 
             <Footer />
